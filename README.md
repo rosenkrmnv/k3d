23e72ca483e5e8820ffb0ce1be6931b12706538f
Rosen Karamanov <rkaramanov@gmail.com>
2025-06-06 13:42:15 +0000

    update README.md and prerequisites.sh

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# k3d
+# Local Kubernetes Cluster Setup with K3d
 
+This guide will walk you through setting up a local Kubernetes cluster with K3d using a Taskfile. The steps include installing K3d and creating a cluster.
+
+---
+
+## Prerequisites (Ubuntu 24.*)
+
+```sh
+./prerequisites.sh
+
+```
+
+---
+
+## Run the Setup
+
+Run the setup task to create and configure the local Kubernetes cluster:
+    ```sh
+    task setup
+    ```
+
+## Tasks Overview
+
+The setup task will perform the following steps:
+
+1. **Install K3d:** Installs K3d for managing the Kubernetes cluster.
+2. **Create Cluster:** Creates a new Kubernetes cluster with K3d.
+3. **Verify Cluster:** Verifies that the Kubernetes cluster is running.
+4. **Switch Context:** Switches the context to the new K3d cluster.
+5. **Print Setup Complete:** Prints a message indicating the setup is complete.
--- prerequisites.sh
+++ prerequisites.sh
@@ -1,4 +1,4 @@
-#install docker
+# Install docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
@@ -15,18 +15,20 @@ echo \
 sudo apt-get update
 
 
-#install helm
+# Install helm
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-#install kubectl
+# Install kubectl
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 curl -LO https://dl.k8s.io/release/v1.33.0/bin/linux/arm64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
-#install fluxcli
-curl -s https://fluxcd.io/install.sh | sudo bash
+# Install task
+
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin/
+
